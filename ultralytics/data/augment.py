5b583f9d220d4600171600530a3ad7872bf1bb80
Dentikka <denis@shveykin.com>
2025-03-21 14:30:39 +0300

    update padding

--- ultralytics/data/augment.py
+++ ultralytics/data/augment.py
@@ -1587,7 +1587,7 @@ class LetterBox:
         top, bottom = int(round(dh - 0.1)) if self.center else 0, int(round(dh + 0.1))
         left, right = int(round(dw - 0.1)) if self.center else 0, int(round(dw + 0.1))
         img = cv2.copyMakeBorder(
-            img, top, bottom, left, right, cv2.BORDER_CONSTANT, value=(114, 114, 114)
+            img, top, bottom, left, right, cv2.BORDER_CONSTANT, value=(114*256, 114*256, 114*256)
         )  # add border
         if labels.get("ratio_pad"):
             labels["ratio_pad"] = (labels["ratio_pad"], (left, top))  # for evaluation
